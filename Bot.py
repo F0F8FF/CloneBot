ce48a26ac2f837f853ffdda3e10df8644306da12
F0F8FF <qkrtmdgml112@naver.com>
2024-04-12 17:58:22 +0900

    코드 주석 추가

--- Bot.py
+++ Bot.py
@@ -23,7 +23,9 @@ st.title('음식 추천 챗봇')
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    st.session_state["messages"] = [
+        ChatMessage(role="assistant", content="오늘은 무엇을 먹을까요?")
+    ]
 
 #채팅 대화기록 저장
 if "store"  not in st.session_state:
@@ -31,7 +33,7 @@ if "store"  not in st.session_state:
 
 with st.sidebar:
     session_id = st.text_input("Session ID", value="abc123")
-
+    #채팅 대화기록 초기화
     clear_btn = st.button("clear")
     if clear_btn:
         st.session_state["messages"] = []
@@ -43,6 +45,7 @@ if "messages" in st.session_state and len(st.session_state["messages"]) > 0:
     for chat_message in st.session_state["messages"]:
         st.chat_message(chat_message.role).write(chat_message.content)
 
+#세션 ID를 기반으로 세션 기록을 가져옴
 def get_session_history(session_ids: str) -> BaseChatMessageHistory:
     if session_ids not in st.session_state["store"]:
         st.session_state["store"][session_ids] = ChatMessageHistory()
@@ -53,11 +56,13 @@ if user_input := st.chat_input("메세지를 입력해주세요."):
     st.chat_message("user").write(f"{user_input}")
     st.session_state["messages"].append(ChatMessage(role="user", content=user_input))
     
+    #AI 답변
     with st.chat_message("assistant"):
         stream_handler = StreamHandler(st.empty())
 
+        #모델 생성
         llm = ChatOpenAI(streaming=True, callbacks=[stream_handler])
-
+        #프롬프트 생성
         prompt = ChatPromptTemplate.from_messages(
             [
                 (
@@ -81,10 +86,10 @@ if user_input := st.chat_input("메세지를 입력해주세요."):
 
         response = chain_with_memory.invoke(
             {"question" : user_input},
+            #세션 ID 설정
             config={"configurable" : {"session_id" : session_id}},
         )
 
-        #AI 답변
         st.session_state["messages"].append(
             ChatMessage(role="assistant", content=response.content)
         )
